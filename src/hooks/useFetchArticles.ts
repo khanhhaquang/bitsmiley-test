01c0ca6e44392ad923cb75553f1c03250453eccc
Eva <17610603221@163.com>
2024-02-04 18:07:46 +0800

    static feed data

--- src/hooks/useFetchArticles.ts
+++ src/hooks/useFetchArticles.ts
@@ -1,17 +1,24 @@
-import { useQuery } from 'react-query'
-import { FeedService } from '@/services/feed'
+// import { useQuery } from 'react-query'
+// import { FeedService } from '@/services/feed'
 
-const imgUrlReg = /https:\/\/.*?(gif|png|jpg)/gi
+// const imgUrlReg = /https:\/\/.*?(gif|png|jpg)/gi
 
 export const useFetchArticles = () => {
-  const { data, isLoading } = useQuery(
-    FeedService.getFeeds.key,
-    FeedService.getFeeds.call
-  )
-  const items = data?.data?.items?.map((i) => ({
-    ...i,
-    img: i?.content?.match(imgUrlReg)?.[0]
-  }))
+  const isLoading = false
+  const items = [
+    {
+      link: 'https://medium.com/@bitsmiley_labs/bitsmiley-discord-wl-all-for-the-horde-83705019801f?source=rss-51d7f98a9f01------2',
+      img: 'https://cdn-images-1.medium.com/max/1024/1*ddTaWCrkNMe3tr6IMMSmlw.png'
+    },
+    {
+      link: 'https://medium.com/@bitsmiley_labs/btc-leading-protocol-introduces-og-nft-bitdisc-6b3684a59615?source=rss-51d7f98a9f01------2',
+      img: 'https://cdn-images-1.medium.com/max/1024/1*q0bHv6V9734rbIacriiAXA.png'
+    },
+    {
+      link: 'https://medium.com/@bitsmiley_labs/fund-protection-bitinsurance-1c008885567a?source=rss-51d7f98a9f01------2',
+      img: 'https://cdn-images-1.medium.com/max/1024/1*Iq78WeFuM5H64dh92uPrmw.png'
+    }
+  ]
 
   return { items, isLoading }
 }
