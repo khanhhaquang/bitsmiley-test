d1798a009f2248a6536b072a03115a062b8dad9e
Khanh Ha <khanhhaquang@gmail.com>
2024-03-20 11:11:32 +0700

    feat: update ui

--- src/components/StakingMachine/BoxContent/ConnectedNotStaked.tsx
+++ src/components/StakingMachine/BoxContent/ConnectedNotStaked.tsx
@@ -36,10 +36,10 @@ export const ConnectedNotStaked: React.FC = () => {
             <span>X???</span>
           </div>
           <StakeButton onClick={() => setIsChooseModalOpen(true)} />
-          <RightAngle className="absolute left-0 top-0 text-green2" />
-          <RightAngle className="absolute right-0 top-0 rotate-90 text-green2" />
-          <RightAngle className="absolute bottom-0 right-0 rotate-180 text-green2" />
-          <RightAngle className="absolute bottom-0 left-0 -rotate-90 text-green2" />
+          <RightAngle className="absolute left-[-1px] top-[-1px] text-cyan" />
+          <RightAngle className="absolute right-[-1px] top-[-1px] rotate-90 text-cyan" />
+          <RightAngle className="absolute bottom-[-1px] right-[-1px] rotate-180 text-cyan" />
+          <RightAngle className="absolute bottom-[-1px] left-[-1px] -rotate-90 text-cyan" />
         </div>
       </div>
     </>
