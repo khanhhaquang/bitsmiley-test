b58d8ae96837c8fc06646e4908b2193fa809fa0c
Khanh Ha <khanhhaquang@gmail.com>
2024-03-20 11:03:47 +0700

    feat: handleClose with switch

--- src/components/WrongNetworkModal.tsx
+++ src/components/WrongNetworkModal.tsx
@@ -1,6 +1,5 @@
 import { useSwitchChain } from 'wagmi'
 
-import { CloseIcon } from '@/assets/icons'
 import { Button } from '@/components/Button'
 import { Image } from '@/components/Image'
 import { Modal } from '@/components/Modal'
@@ -31,16 +30,13 @@ const WrongNetworkModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
   }
 
   const handleClose = () => {
+    handleSwitch(chains[0].id)
     onClose()
   }
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
       <div className="relative border-2 border-black bg-black bg-connect-modal bg-cover bg-no-repeat font-smb text-2xl">
-        <CloseIcon
-          onClick={handleClose}
-          className="absolute right-2.5 top-2.5 cursor-pointer"
-        />
         <div className="flex flex-col items-center gap-y-9 p-6 pt-[42px]">
           <div className="whitespace-nowrap">network switch</div>
           <div className="flex w-[361px] flex-col items-center gap-y-6">
--- src/config/chain.ts
+++ src/config/chain.ts
@@ -2,6 +2,6 @@ import { getIllustrationUrl } from '@/utils/getAssetsUrl'
 
 export const chainsIconUrl: { [key: string]: string } = {
   4200: getIllustrationUrl('merlin-chain-logo', 'webp'),
-  686868: getIllustrationUrl('merlin-chain-logo', 'webp'),
+  686868: getIllustrationUrl('bob-chain-logo', 'webp'),
   111: getIllustrationUrl('bob-chain-logo', 'webp')
 } as const
