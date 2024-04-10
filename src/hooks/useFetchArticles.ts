c5ad6d659ef9e31695c533ac7d2396ba5958b851
chainbyblock <92616834+chainbyblock@users.noreply.github.com>
2024-04-10 15:41:33 +0800

    Fix main page url (#174)

--- src/hooks/useFetchArticles.ts
+++ src/hooks/useFetchArticles.ts
@@ -7,16 +7,16 @@ export const useFetchArticles = () => {
   const isLoading = false
   const items = [
     {
-      link: 'https://medium.com/@bitsmiley_labs/bitsmiley-discord-wl-all-for-the-horde-83705019801f?source=rss-51d7f98a9f01------2',
-      img: 'https://cdn-images-1.medium.com/max/1024/1*ddTaWCrkNMe3tr6IMMSmlw.png'
+      link: 'https://medium.com/@bitsmiley/the-trumeme-show-bitcow-x-bitsmiley-joint-public-testnet-event-37347ecfb2ce',
+      img: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*2mcj9rDaXamuyYnwAhtiXA.png'
     },
     {
-      link: 'https://medium.com/@bitsmiley_labs/btc-leading-protocol-introduces-og-nft-bitdisc-6b3684a59615?source=rss-51d7f98a9f01------2',
-      img: 'https://cdn-images-1.medium.com/max/1024/1*q0bHv6V9734rbIacriiAXA.png'
+      link: 'https://medium.com/@bitsmiley/btc-leading-protocol-introduces-og-nft-bitdisc-f876ccae5a56',
+      img: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/0*AzspLFwctgHyJkEt.png'
     },
     {
-      link: 'https://medium.com/@bitsmiley_labs/fund-protection-bitinsurance-1c008885567a?source=rss-51d7f98a9f01------2',
-      img: 'https://cdn-images-1.medium.com/max/1024/1*Iq78WeFuM5H64dh92uPrmw.png'
+      link: 'https://medium.com/@bitsmiley/bitbook-the-btc-pilots-handbook-part-1-0ce464b06139',
+      img: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*liG1gNKP5HHPhnWuFbvsbw.png'
     }
   ]
 
