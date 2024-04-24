b988cb9083553e5fbcf337181d3e39b2150e489e
Khanh Ha <khanhhaquang@gmail.com>
2024-04-24 19:04:39 +0700

    feat: remove log

--- src/hooks/useCollaterals.ts
+++ src/hooks/useCollaterals.ts
@@ -151,8 +151,6 @@ export const useCollaterals = (chainId: number, collateralId?: string) => {
               data: collateralsRes
             })
 
-            console.log('🚀 ~ : ~ collaterals:', collaterals)
-
             // vaultAddress
             const vaultAddressRes = await client.request({
               method: 'eth_call',
