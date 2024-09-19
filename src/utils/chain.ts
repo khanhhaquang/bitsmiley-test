87d550a9977e078ebf2dbde9fb28ecc258de547d
Vu Phan <vuphan365@gmail.com>
2024-09-19 18:31:58 +0700

    fix: 🐛 remove unused code

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -1,11 +1,5 @@
-import { customChains, zetaMainnet, zetaTestnet } from '@/config/wagmi'
+import { zetaMainnet, zetaTestnet } from '@/config/wagmi'
 
 export const isZetaChain = (chain: number) => {
   return ([zetaTestnet.id, zetaMainnet.id] as number[]).includes(chain)
-}
-
-export const isTestnet = (chain: number) => {
-  const chainInfo = customChains.find((item) => item.id === chain)
-  if (!chainInfo) return true
-  return chainInfo.testnet
 }
