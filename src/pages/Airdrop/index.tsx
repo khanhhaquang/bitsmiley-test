2a8e0b5c2505d01151a99ddd42b9cefb058dc8cf
Vu Phan <vuphan365@gmail.com>
2024-10-07 15:46:01 +0700

    feat: 🎸 Init airdrop page

---
+++ src/pages/Airdrop/index.tsx
@@ -0,0 +1,11 @@
+const Airdrop = () => {
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-cover bg-center bg-no-repeat text-white">
+      <div className="flex size-full flex-col items-center justify-center">
+        Airdrop
+      </div>
+    </div>
+  )
+}
+
+export default Airdrop
