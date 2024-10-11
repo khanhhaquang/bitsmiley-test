a5a65d8c218e0c5d076504a5bcfcc9d0ce53cdc9
Vu Phan <vuphan365@gmail.com>
2024-10-11 21:43:28 +0700

    fix: 🐛 Wrap journey data by useMemo

--- src/pages/Airdrop/components/YourBitsmileyJourney.tsx
+++ src/pages/Airdrop/components/YourBitsmileyJourney.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 
 import { useGetMyBitsmileyJourney } from '@/hooks/useAirdropQueries'
 import { useMediaQuery } from '@/hooks/useMediaQuery'
@@ -13,8 +13,20 @@ const YourBitsmileyJourney = () => {
   const cardsRef = useRef<HTMLDivElement>(null)
   const { isMobile } = useMediaQuery()
 
-  const communityJourney = data?.find(
-    (item) => item?.type === BitsmileyJourneyType.SPECIAL_COMMUNITY_EVENTS
+  const communityJourney = useMemo(
+    () =>
+      data?.find(
+        (item) => item?.type === BitsmileyJourneyType.SPECIAL_COMMUNITY_EVENTS
+      ),
+    [data]
+  )
+
+  const listNormalJourney = useMemo(
+    () =>
+      data?.filter(
+        (item) => item?.type !== BitsmileyJourneyType.SPECIAL_COMMUNITY_EVENTS
+      ),
+    [data]
   )
 
   const onWheel = (event: WheelEvent) => {
@@ -49,26 +61,21 @@ const YourBitsmileyJourney = () => {
         ref={cardsRef}
         className="scrollbar-none m-auto flex w-full flex-col gap-y-[60px] overflow-x-auto sm:items-center sm:p-0 md:px-[100px] lg:px-[225px]">
         <div className="flex w-fit flex-nowrap items-center pb-1 sm:flex-col">
-          {data
-            ?.filter(
-              (item) =>
-                item?.type !== BitsmileyJourneyType.SPECIAL_COMMUNITY_EVENTS
-            )
-            ?.map((item, index) => (
-              <>
-                {index !== 0 && (
-                  <img
-                    src={getIllustrationUrl('next-journey-chevron', 'webp')}
-                    className="size-10 px-1 sm:rotate-90"
-                  />
-                )}
-                <BitsmileyJourneyCard
-                  key={item?.type}
-                  {...item}
-                  name={BitsmileyJourneyNames[item?.type]}
+          {listNormalJourney?.map((item, index) => (
+            <>
+              {index !== 0 && (
+                <img
+                  src={getIllustrationUrl('next-journey-chevron', 'webp')}
+                  className="size-10 px-1 sm:rotate-90"
                 />
-              </>
-            ))}
+              )}
+              <BitsmileyJourneyCard
+                key={item?.type}
+                {...item}
+                name={BitsmileyJourneyNames[item?.type]}
+              />
+            </>
+          ))}
         </div>
       </div>
       {communityJourney && <CommunityJourneyCard {...communityJourney} />}
