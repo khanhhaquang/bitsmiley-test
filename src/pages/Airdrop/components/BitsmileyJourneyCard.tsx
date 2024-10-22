29bd79d44b6804e11cce66d3417524759789aa2e
Khanh Ha <khanhhaquang@gmail.com>
2024-10-22 20:35:02 +0700

    feat: my journey modal

---
+++ src/pages/Airdrop/components/BitsmileyJourneyCard.tsx
@@ -0,0 +1,86 @@
+import { FC } from 'react'
+
+import { BitsmileyJourney, BitsmileyJourneyStatus } from '@/services/airdrop'
+import { cn } from '@/utils/cn'
+import { getIllustrationUrl } from '@/utils/getAssetsUrl'
+import { formatNumberAsTrunc } from '@/utils/number'
+
+export const getJourneyStatusTitle = (
+  status: BitsmileyJourneyStatus,
+  airdropAmount: number
+) => {
+  switch (status) {
+    case BitsmileyJourneyStatus.NOT_ENTITLED:
+      return 'You are not entitled'
+    case BitsmileyJourneyStatus.COMING_SOON:
+      return 'Coming soon'
+    case BitsmileyJourneyStatus.ACTIVE:
+      return formatNumberAsTrunc(airdropAmount || '')
+    default:
+      return ''
+  }
+}
+
+const BitsmileyJourneyCard: FC<BitsmileyJourney> = ({
+  name,
+  airdropAmount,
+  mediumLink,
+  status
+}) => {
+  const isNotEntitled = status === BitsmileyJourneyStatus.NOT_ENTITLED
+
+  return (
+    <div className="relative h-[162px] w-[274px] shrink-0 border-[1.5px] border-[#2648ef]/60 bg-[#000727] p-1 font-ibmr">
+      <div className="relative z-[1] flex size-full flex-col justify-between">
+        <div
+          className={cn(
+            'flex w-full capitalize items-center justify-center bg-blue p-[10px] text-center text-base text-white h-[90px] leading-6 font-bold',
+            { 'opacity-40': isNotEntitled }
+          )}>
+          {name}
+        </div>
+        <div className="flex size-full flex-1 items-end justify-center bg-cover text-sm">
+          <a
+            href={mediumLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cn('text-[#FFB800] underline', {
+              'text-white': isNotEntitled
+            })}>
+            Rules
+          </a>
+        </div>
+        <div className="flex h-[60px] w-full flex-col text-sm font-bold uppercase">
+          <div
+            className={cn('bg-repeat flex-1 flex justify-center pt-0.5')}
+            style={{
+              backgroundImage: `url(${getIllustrationUrl(
+                isNotEntitled
+                  ? 'journey-card-inactive-bg'
+                  : 'journey-card-active-bg',
+                'webp'
+              )})`
+            }}>
+            <div
+              className={cn('bg-[#120E1F] text-center px-5 text-[#FFB800]', {
+                'text-[#8A8A8A]': isNotEntitled
+              })}>
+              airdrops
+            </div>
+          </div>
+          <div
+            className={cn(
+              'bg-[#FFB800] flex-1 text-center text-black w-full py-1',
+              {
+                'bg-[#8A8A8A]': isNotEntitled
+              }
+            )}>
+            {getJourneyStatusTitle(status, airdropAmount)}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BitsmileyJourneyCard
---
+++ src/pages/Airdrop/components/MyJourneyModal.tsx
@@ -0,0 +1,107 @@
+import { FC, Fragment, useEffect, useMemo, useRef } from 'react'
+
+import { Modal } from '@/components/Modal'
+import { useGetMyBitsmileyJourney } from '@/hooks/useAirdropQueries'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
+import { BitsmileyJourneyNames, BitsmileyJourneyType } from '@/services/airdrop'
+import { getIllustrationUrl } from '@/utils/getAssetsUrl'
+
+import BitsmileyJourneyCard from './BitsmileyJourneyCard'
+import CommunityJourneyCard from './CommunityJourneyCard'
+import { CloseIcon } from '@/assets/icons'
+
+const MyJourneyModal: FC<{
+  isOpen: boolean
+  onClose: () => void
+}> = ({ isOpen, onClose }) => {
+  const { data } = useGetMyBitsmileyJourney()
+  const cardsRef = useRef<HTMLDivElement>(null)
+  const { isMobile } = useMediaQuery()
+
+  const communityJourney = useMemo(
+    () =>
+      data?.find(
+        (item) => item?.type === BitsmileyJourneyType.SPECIAL_COMMUNITY_EVENTS
+      ),
+    [data]
+  )
+
+  const listNormalJourney = useMemo(
+    () =>
+      data?.filter(
+        (item) => item?.type !== BitsmileyJourneyType.SPECIAL_COMMUNITY_EVENTS
+      ),
+    [data]
+  )
+
+  const onWheel = (event: WheelEvent) => {
+    if (event?.deltaY !== 0 && cardsRef?.current) {
+      const { scrollWidth, scrollLeft, clientWidth, scrollTop } =
+        cardsRef.current
+      const maxScrollWidth = scrollWidth - clientWidth
+      const calculatedScrollWidth = scrollLeft + event.deltaY * 5
+
+      const isPositionBetweenScroll = (position: number) =>
+        position > 0 && position < maxScrollWidth
+
+      if (
+        isPositionBetweenScroll(scrollLeft) ||
+        isPositionBetweenScroll(calculatedScrollWidth)
+      ) {
+        // do horizontal scroll until it reach to maximum width, then do vertical scroll
+        cardsRef.current?.scroll(calculatedScrollWidth, scrollTop)
+        event.preventDefault()
+      }
+    }
+  }
+
+  useEffect(() => {
+    const currentCardRef = cardsRef.current
+    if (!isMobile) {
+      currentCardRef?.addEventListener('wheel', onWheel)
+    }
+    return () => {
+      currentCardRef?.removeEventListener('wheel', onWheel)
+    }
+  }, [isMobile])
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} backdrop={false}>
+      <div className="relative flex w-[794px] flex-col bg-black py-9">
+        <button onClick={onClose} className="absolute right-4 top-4 text-white">
+          <CloseIcon width={18} height={17} />
+        </button>
+        <div className="flex w-full items-center justify-center">
+          <img
+            width={462}
+            height={80}
+            src={getIllustrationUrl('your-bitsmiley-journey', 'webp')}
+          />
+        </div>
+        <div
+          ref={cardsRef}
+          className="scrollbar-none mt-9 flex w-full flex-nowrap items-center overflow-x-auto overflow-y-hidden pl-[94px] md:pr-10 lg:pr-20">
+          {listNormalJourney?.map((item, index) => (
+            <Fragment key={`${index}-${item?.type}`}>
+              {index !== 0 && (
+                <img
+                  src={getIllustrationUrl('next-journey-chevron', 'webp')}
+                  className="size-[30px] px-1 sm:rotate-90"
+                />
+              )}
+              <BitsmileyJourneyCard
+                {...item}
+                name={BitsmileyJourneyNames[item?.type]}
+              />
+            </Fragment>
+          ))}
+        </div>
+        <div className="mt-3 w-full">
+          {communityJourney && <CommunityJourneyCard {...communityJourney} />}
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export default MyJourneyModal
