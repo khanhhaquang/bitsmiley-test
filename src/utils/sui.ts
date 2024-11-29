010ef838a41786baa8c6e8e115f60eaf0665eee0
Vu Phan <vuphan365@gmail.com>
2024-11-29 22:42:04 +0700

    fix: 🐛 Simple function utils

--- src/utils/sui.ts
+++ src/utils/sui.ts
@@ -3,21 +3,16 @@ import { keccak256, toHex } from 'viem'
 
 import { BcsI64 } from '@/types/sui'
 
-export const convertToMist = (amount: number) => {
-  return (BigInt(Number(MIST_PER_SUI) * amount) * MIST_PER_SUI) / MIST_PER_SUI
-}
+export const convertToMist = (amount: number) =>
+  (BigInt(Number(MIST_PER_SUI) * amount) * MIST_PER_SUI) / MIST_PER_SUI
 
-export const parseFromMist = (amount: bigint) => {
-  return Number(amount / MIST_PER_SUI)
-}
+export const parseFromMist = (amount: bigint) => Number(amount / MIST_PER_SUI)
 
-export const collateralHash = (collateral: string) => {
-  return keccak256(toHex(collateral))
-}
+export const collateralHash = (collateral: string) =>
+  keccak256(toHex(collateral))
 
-export const toI64 = (value: bigint) => {
-  return BcsI64.serialize({
+export const toI64 = (value: bigint) =>
+  BcsI64.serialize({
     value: Math.abs(Number(value)),
     is_negative: Number(value) < 0
   })
-}
